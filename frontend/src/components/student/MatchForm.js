8e07c0f3d87fe48082a4451d6facfbc7379fd8fc
Adammangzijun <117640074+adammangzijun@users.noreply.github.com>
2024-11-06 19:33:18 +0800

    Proposed Final Design for Main Page

--- frontend/src/components/student/MatchForm.js
+++ frontend/src/components/student/MatchForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import categories from "../../config/categoryConfig";
-import startBackground from "../../assets/start-background.jpg"
+import background from "../../assets/background.jpg";
 
 const MatchForm = ({ onSubmit }) => {
   const [category, setCategory] = useState([]);
@@ -27,86 +27,80 @@ const MatchForm = ({ onSubmit }) => {
 
   return (
     <form id="matchForm" onSubmit={handleSubmit}>
-      <div class="flex h-screen">
-        <div class="flex flex-col w-1/2 justify-end items-start bg-gray-800 p-8 text-white">
-          <p className="text-5xl text-[#0F0A3C] font-bold m-4">
-              PEERPREP
-          </p>
-
-          <p className="flex text-xl text-[#0F0A3C] text-center p-8">
-            Ready to find your perfect match? Click the button to start now!
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        class="flex h-screen"
+      >
+        <div class="flex flex-col w-1/2 justify-center items-start bg-gray-800 bg-opacity-0 pl-16 text-white">
+          <p className="text-9xl text-white font-bold ">CONNECT</p>
+          <p className="text-9xl text-blue-200 font-bold ">CODE</p>
+          <p className="text-9xl text-blue-400 font-bold ">CREATE</p>
+          <p className="flex text-2xl text-white my-2">
+            Start your journey with a peer who's just right for you.
           </p>
         </div>
 
-        <div class="flex flex-col w-1/2 bg-white p-8 text-gray-800">
-          <div 
-            className="flex items-center justify-center my-4"
-          >
-            <button type="submit" className="btn justify-center align-middle p-4 font-bold text-3xl rounded-full h-48 w-48 my-8">
-              START
-            </button>
-          </div>
-
-          {/* for the translucent background, one for category, one for difficulty */}
-          <div class="bg-white/20 backdrop-blur-md text-white p-4 rounded-lg shadow-lg w-48">
-
-          </div>
-
-          <div className="flex flex-col form-group mb-4 mt-4">
-            <div className="col">
-              <label className="gray-label flex justify-center items-align text-xl font-semibold mb-2" htmlFor="category">
+        <div class="flex flex-col justify-center items-center w-1/2 bg-opacity-0 p-8 text-gray-800">
+          <div className="flex flex-col justify-center w-3/4 items-center bg-blue-200 bg-opacity-50 backdrop-blur-md p-8 text-gray-800 rounded-4 space-y-4"> {/* New container for layout */}
+            {/* Right-aligned Category */}
+            <div className="flex flex-col justify-center text-white p-2 w-full">
+              <label
+                className="gray-label flex justify-center text-xl font-semibold mb-2"
+                htmlFor="category"
+              >
                 Category
               </label>
-
-              <div className="row-md-8">
-                <div className="multi-select">
-                  <select
-                    id="categories"
-                    defaultValue=""
-                    onChange={handleCategoryChange}
-                    className="block mx-auto py-3 px-5 text-center w-[75%] rounded-lg border-1 border-black bg-white text-black focus:outline-none text-lg"
-                    // required
-                  >
-                    <option value="" disabled>
-                      Please select a topic
+              <div className="multi-select">
+                <select
+                  id="categories"
+                  defaultValue=""
+                  onChange={handleCategoryChange}
+                  className="block mx-auto py-3 text-center w-full rounded-lg shadow-lg bg-blue-50 backdrop-blur-md bg-opacity-80 text-black focus:outline-none text-lg"
+                >
+                  <option value="" disabled className="bg-blue-50 text-black">
+                    Please select a topic
+                  </option>
+                  {categories.map((cat) => (
+                    <option key={cat.value} value={cat.value}>
+                      {cat.label}
                     </option>
-                    {/* Use categories from config file */}
-                    {categories.map((cat) => (
-                      <option key={cat.value} value={cat.value}>
-                        {cat.label}
-                      </option>
-                    ))}
-                  </select>
-
-                  <div className="selected-categories">
-                    {category.map((cat) => (
-                      <span key={cat} className="tag bg-grey">
-                        {cat}
-                        <button
-                          type="button"
-                          onClick={() => removeCategory(cat)}
-                          className="remove-tag"
-                        >
-                          ×
-                        </button>
-                      </span>
-                    ))}
-                  </div>
+                  ))}
+                </select>
+                <div className="selected-categories">
+                  {category.map((cat) => (
+                    <span key={cat} className="tag bg-grey">
+                      {cat}
+                      <button
+                        type="button"
+                        onClick={() => removeCategory(cat)}
+                        className="remove-tag"
+                      >
+                        ×
+                      </button>
+                    </span>
+                  ))}
                 </div>
               </div>
             </div>
 
-            <div className="col">
-              <label className="gray-label flex justify-center items-align text-xl font-semibold mb-2" htmlFor="difficulty">
+            {/* Left-aligned Difficulty */}
+            <div className="flex flex-col text-white p-2 w-full">
+              <label
+                className="gray-label flex justify-center text-xl font-semibold mb-2"
+                htmlFor="difficulty"
+              >
                 Difficulty
               </label>
               <select
                 type="text"
-                className="block mx-auto py-3 px-5 text-center w-[75%] rounded-lg border-1 border-black bg-white text-black focus:outline-none text-lg"
+                className="block mx-auto py-3 text-center w-full rounded-lg shadow-lg bg-blue-50 backdrop-blur-md bg-opacity-80 text-black focus:outline-none text-lg"
                 id="difficulty"
                 name="difficulty"
                 value={difficulty}
-                placeholder="difficulty"
                 onChange={(e) => setDifficulty(e.target.value)}
                 required
               >
@@ -117,6 +111,14 @@ const MatchForm = ({ onSubmit }) => {
                 <option value="Medium">Medium</option>
                 <option value="Hard">Hard</option>
               </select>
+            </div>
+            <div className="flex justify-center mt-2">
+              <button
+                type="submit"
+                className="btn justify-center align-middle p-4 font-bold text-3xl rounded-full h-48 w-48 my-8"
+              >
+                START
+              </button>
             </div>
           </div>
         </div>
