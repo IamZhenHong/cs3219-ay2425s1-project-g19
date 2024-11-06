5e469bc15102d01fec92ad87ed2009b85c848c69
IamZhenHong <109903639+IamZhenHong@users.noreply.github.com>
2024-11-06 15:30:11 +0800

    Fix match cancelling

--- backend/matching-service/routes/matching.js
+++ backend/matching-service/routes/matching.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const pushReq = require('../controllers/matchingControllers');
+const { pushReq, cancelMatch } = require('../controllers/matchingControllers');
 
 // Matching Service
-router.post('/', pushReq);
+router.post('/', pushReq); // Ensure pushReq is a function
+router.post('/cancel', cancelMatch); // Ensure cancelMatch is a function
 
 module.exports = router;
