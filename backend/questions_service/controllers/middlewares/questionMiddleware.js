95481f9ccd548292a326375d39f93228228e907b
Leb14 <haocj98@gmail.com>
2024-11-08 10:06:56 +0800

    Add to assign question functionality

--- backend/questions_service/controllers/middlewares/questionMiddleware.js
+++ backend/questions_service/controllers/middlewares/questionMiddleware.js
@@ -17,6 +17,33 @@ const getQuestionById = async (req, res, next) => {
     next();
 }
 
+const getQuestionByCriteria = async (req, res, next) => {
+    const { difficulty, categories } = req.params; 
+
+    let questions;
+    
+    try {
+        const categoryArray = categories.split(',').map(category => category.trim());
+
+        questions = await Question.find({
+            complexity: { $regex: new RegExp(difficulty, "i") },  
+            category: { $in: categoryArray.map(cat => new RegExp(cat, "i")) }  
+        });
+
+        if (questions.length === 0) {
+            return res.status(404).json({ message: 'No questions found for the given criteria' });
+        }
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+
+    res.question = questions;
+    next();
+};
+
+
+
 const checkDuplicateTitle = async (req, res, next) => {
     const title = req.body.title;
     try {
@@ -32,4 +59,4 @@ const checkDuplicateTitle = async (req, res, next) => {
     next();
 }
 
-module.exports = {getQuestionById, checkDuplicateTitle};
+module.exports = {getQuestionById, checkDuplicateTitle, getQuestionByCriteria};
